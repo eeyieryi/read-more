ee42b063903589b3dbe8f24e1894a88e843f4052
Emanuel <152207479+eeyieryi@users.noreply.github.com>
2023-11-30 21:06:56 +0400

    fix not showing button to create entry

--- src/routes/entries/new/+page.svelte
+++ src/routes/entries/new/+page.svelte
@@ -128,32 +128,34 @@
 				name="transcription"
 				bind:value="{createOneDto.transcription}"></textarea>
 		</div>
-		<div class="mb-4 flex flex-row items-center">
-			<label
-				class="mr-2 text-lg font-bold uppercase text-gray-900"
-				for="collection">existing collection</label>
-			<select
-				class="px-3 py-2 text-lg text-gray-900"
-				id="collection"
-				name="collection"
-				bind:value="{createOneDto.collectionId}">
-				<option value="">-</option>
-
-				{#each data.collections as collection (collection.id)}
-					<option value="{collection.id}">{collection.title}</option>
-				{/each}
-			</select>
-		</div>
-		<div class="mb-8 flex flex-col">
-			<label
-				class="mb-2 text-lg font-bold uppercase text-gray-900"
-				for="new-collection">new collection</label>
-			<input
-				class="border px-3 py-2"
-				id="new-collection"
-				name="new-collection"
-				type="text"
-				bind:value="{createOneDto.collectionTitle}" />
+		<div class="mb-8 flex w-full flex-row">
+			<div class="mr-16 flex flex-row items-center">
+				<label
+					class="mr-8 text-lg font-bold uppercase text-gray-900"
+					for="collection">existing collection</label>
+				<select
+					class="px-16 py-2 text-lg text-gray-900"
+					id="collection"
+					name="collection"
+					bind:value="{createOneDto.collectionId}">
+					<option value="">-</option>
+					{#each data.collections as collection (collection.id)}
+						<option value="{collection.id}"
+							>{collection.title}</option>
+					{/each}
+				</select>
+			</div>
+			<div>
+				<label
+					class="mb-2 text-lg font-bold uppercase text-gray-900"
+					for="new-collection">new collection</label>
+				<input
+					class="border px-3 py-2"
+					id="new-collection"
+					name="new-collection"
+					type="text"
+					bind:value="{createOneDto.collectionTitle}" />
+			</div>
 		</div>
 		<div class="mt-auto flex flex-row">
 			<button class="ml-auto block border px-3 py-2 text-lg" type="submit"
